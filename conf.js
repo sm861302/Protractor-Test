56e3f0a99c48a48e405ea460ca3bfe0c1f1bc4ca
Shubham <smajumdar22@gmail.com>
2020-01-10 03:29:06 -0800

    Change conf file

---
+++ conf.js
@@ -0,0 +1,5 @@
+exports.config = {
+    directConnect: true,
+    specs: ['spec.js']
+  };
+  
--- spec.js
+++ spec.js
@@ -1,5 +1,5 @@
-describe('Enter GURU99 Name', function() {
-    it('should add a Name as GURU99', function() {
+describe('Enter name feature', function() {
+    it('should add a Name as Test', function() {
     browser.get('https://angularjs.org');
     element(by.model('yourName')).sendKeys('GURU99');
      var guru= element(by.xpath('html/body/div[2]/div[1]/div[2]/div[2]/div/h1'));
